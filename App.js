db420a7a61f808a947796ec60fb3e7df53349cfd
Lydia Gregory <ljg2gb@virginia.edu>
2020-10-20 14:45:53 -0400

    creates component file structure

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Home from './src/pages/Home'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bytelion reviews app starter code</Text>
+      <Home/>
+      <Text>Bytelion reviews app loading...</Text>
       <StatusBar style="auto" />
     </View>
   );
