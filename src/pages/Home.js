88bbada3671e93d9ff01f1c4a688ccdc6e744aa1
Lydia Gregory <ljg2gb@virginia.edu>
2020-10-20 15:45:56 -0400

    creates review component, fetches reviews, adds reviews to state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
+import { TouchableHighlight } from 'react-native-gesture-handler';
+
+import Reviews from '../components/Reviews'
+const url = 'https://my-json-server.typicode.com/bytelion/expo_test_mock_api/reviews'
 
 export default function Home() {
+    const [reviews, setReviews] = useState([]);
+
+    const displayReviews = () => {
+        fetch(url)
+            .then(response => response.json())
+            .then(result => setReviews(result))
+            .then(console.log(reviews))
+    }
+
     return(
         <View>
             <Text>Home Page</Text>
+            <TouchableHighlight 
+                underlayColor='rgb(210,220,230)' 
+                onPress={displayReviews}
+            >
+                    <Text>button</Text>
+            </TouchableHighlight>
+            <Reviews/>
         </View>
     )
 }
--- src/routes/Index.js
+++ src/routes/Index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// import 'react-native-gesture-handler';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from '../pages/Home';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
 export default function Index() {
     return(
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name={"Home"} component={Home} options={{ title: 'Home Page' }} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Home/>
+        // <NavigationContainer>
+        //     <Stack.Navigator>
+        //         <Stack.Screen name={"Home"} component={Home} options={{ title: 'Home Page' }} />
+        //     </Stack.Navigator>
+        // </NavigationContainer>
     )
 }
